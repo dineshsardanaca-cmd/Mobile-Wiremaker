379ad3cb7429f7ff18f0de567b0ae669f6b8b94a
Dinesh Sardana <dinesh.sardana.ca@gmail.com>
2026-04-04 18:06:39 -0700

    Backup src/components/custom-icons.tsx from MenuMorph App

---
+++ src/components/custom-icons.tsx
@@ -0,0 +1,69 @@
+
+import type { SVGProps } from 'react';
+
+export function NutFreeIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M17.11 11.15A4.33 4.33 0 0 0 12.8 5.5a4.33 4.33 0 0 0-4.31 5.65" />
+      <path d="M12.8 5.5C12.8 5.5 12.8 2 12 2s-.8 3.5-.8 3.5" />
+      <path d="M10.88 16.07c-1.12.8-2.67 1.43-4.14 1.43-3.22 0-5.74-2.58-5.74-5.75S3.38 6 6.6 6c2.43 0 4.5 1.57 5.37 3.75" />
+      <path d="M16.48 10.43c1.39 0 2.52 1.12 2.52 2.5s-1.13 2.5-2.52 2.5c-1.39 0-2.52-1.12-2.52-2.5s1.13-2.5 2.52-2.5z" />
+      <path d="m2 2 20 20" />
+    </svg>
+  );
+}
+
+export function GlutenFreeIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M2 22 22 2" />
+      <path d="M11.25 5.27A5.25 5.25 0 0 0 6 10.52v8.96" />
+      <path d="M12.75 5.27a5.25 5.25 0 0 1 5.25 5.25v8.96" />
+      <path d="M12 14a2.5 2.5 0 0 1-2.5-2.5 2.5 2.5 0 0 1 5 0A2.5 2.5 0 0 1 12 14Z" />
+      <path d="M12 5.45s.2-3.45-1.5-3.45-1.5 3.45-1.5 3.45" />
+    </svg>
+  );
+}
+
+export function SpicyIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+    >
+        <path d="M14.5 12.5c2-2.5.5-6-2-8-2.5-2-5.5-.5-7.5 2s-.5 5.5 2 7.5c2.5 2 6 .5 8-2z" />
+        <path d="M12 15a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+        <path d="M22 6c-2.5-2-5.5-.5-7.5 2" />
+    </svg>
+  );
+}
